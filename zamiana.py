015dc9ff11096f29bf50d87bda3c49f04e3da747
krykowuno <157737115+krykowuno@users.noreply.github.com>
2024-01-26 20:35:39 +0100

    Update zamiana.py

--- zamiana.py
+++ zamiana.py
@@ -4,7 +4,7 @@ def horner(x):
     system=int(x[1])
     w = int(liczba[0])
     for a in range(1,len(liczba)):
-        w+=(system*w)+int(liczba[a])
+        w=(system*w)+int(liczba[a])
     return w
 
 plik=open('zamiana.txt','r')
