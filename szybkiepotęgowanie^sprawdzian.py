05fb81c917e0950eb552913e455a22e1ec6555b5
krykowuno <157737115+krykowuno@users.noreply.github.com>
2024-01-30 09:56:37 +0100

    odpowiedź na maxa

--- szybkiepotęgowanie^sprawdzian.py
+++ szybkiepotęgowanie^sprawdzian.py
@@ -1,11 +1,11 @@
 def potegowanie(lista):
     lista=lista.strip().split()
     potęga=int(lista[1])
-    x=int(lista[0])
+    x=lista[0]
     potega=bin(potęga)[2:]
     potega=potega[::-1]
+    podstawa=int(x)
     wynik=1
-    podstawa=x
     l=1
     for i in potega:
         l+=1
@@ -15,9 +15,12 @@ def potegowanie(lista):
             wynik*=podstawa
             podstawa*=podstawa
 
-    return wynik, "liczba mnożeń "+str(l)
+    return str(lista[0])+"^"+str(lista[1])+"="+str(wynik),"liczba mnożeń"+ " "+ str(l)
 
 plik=open('potegowanie.txt','r')
 linijki=plik.readlines()
 for linijka in linijki:
-    print(potegowanie(linijka))
+    wartosci=linijka.split()
+    print(potegowanie(linijka)[0]+","+potegowanie(linijka)[1])
+
+
